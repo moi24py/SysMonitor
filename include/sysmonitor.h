9b4f30097e405792e1426e7a55327d521162b3ba
moi24py <moi24py@gmail.com>
2026-07-11 12:42:47 +0200

    aggiungi dichiarazione della funzione per calcolare tempo di attività e idle, e la dichiarazione della funzione per calcolare l'utilizzo totale della CPU

--- include/sysmonitor.h
+++ include/sysmonitor.h
@@ -16,11 +16,23 @@ typedef struct stat{
     unsigned long long guest_nice;
 } stat_t;
 
+typedef struct {
+    unsigned long long total;
+    unsigned long long idle;
+} stat_tot_idle_t;
+
+
 /* Funzione che legge la prima riga del file proc/stat,
  e ritorna un oggetto di tipo stat_t con i valori letti */
 stat_t* get_stats(FILE *file);
 
 // Funzione che stampa l'oggetto statistiche
 void print_stats(stat_t* stats);
+
+// Calcolo tempi: totale e idle
+stat_tot_idle_t* sum_total(stat_t *stats);
+
+// // Calcolo utilizzo totale CPU
+double total_CPU_time(stat_tot_idle_t* sum1, stat_tot_idle_t* sum2);
 
 #endif
