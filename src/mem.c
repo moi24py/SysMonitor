0469fa50770a32978132472435db877ece789d3e
moi24py <moi24py@gmail.com>
2026-07-13 17:25:48 +0200

    add basic function to get memory data

---
+++ src/mem.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include "../include/sysmonitor.h"
+
+int parse_mem(mem_stat_t *mem){
+    FILE *fp = fopen("/proc/meminfo", "r");
+    if (fp == NULL){
+        perror("Error: failed to open /proc/meminfo [mem.c parse_mem()]");
+        exit(EXIT_FAILURE);
+    }
+
+    char line[512];
+    int read = 0;
+    while( fgets(line, sizeof(line), fp) != NULL) {
+        if (strncmp(line, "Mem", 3) == 0){
+            read = sscanf(line, "%*s %llu %*s", &(mem->mem_total)); // TO FIX: scrittura nello stesso campo
+        }
+        if (read < 1){
+            perror("Error: parsing failed [mem.c parse_mem()]");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Testa la causa del fallimento della lettura
+    if (ferror(fp)) {
+        perror("fgets");
+        fclose(fp);
+        return EXIT_FAILURE;
+    }
+
+	// Chiusura del file e gestione dell'eventuale fallimento
+    if (fclose(fp) != 0) {
+        perror("fclose");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
