466219cbebc06d215540e2d0e3e5f2aedc642a2d
moi24py <moi24py@gmail.com>
2026-07-14 15:30:56 +0200

    add function that gets and parses  memory info

--- src/mem.c
+++ src/mem.c
@@ -15,9 +15,13 @@ int parse_mem(mem_stat_t *mem){
     char line[512];
     int read = 0;
     while( fgets(line, sizeof(line), fp) != NULL) {
-        if (strncmp(line, "Mem", 3) == 0){
-            read = sscanf(line, "%*s %llu %*s", &(mem->mem_total)); // TO FIX: scrittura nello stesso campo
-        }
+        if (strncmp(line, "MemTotal", 8) == 0)
+            read = sscanf(line, "%*s %llu %*s", &(mem->mem_total));
+        else if(strncmp(line, "MemFree", 7) == 0)
+            read = sscanf(line, "%*s %llu %*s", &(mem->mem_free));
+        else if (strncmp(line, "MemAvailable", 12) == 0)
+            read = sscanf(line, "%*s %llu %*s", &(mem->mem_available));
+        
         if (read < 1){
             perror("Error: parsing failed [mem.c parse_mem()]");
             exit(EXIT_FAILURE);
