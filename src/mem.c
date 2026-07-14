a1fd89b8b394803f1f2bbbd14491b751e315a4da
moi24py <moi24py@gmail.com>
2026-07-14 16:07:05 +0200

    Changed the language used in the comments

--- src/mem.c
+++ src/mem.c
@@ -5,13 +5,14 @@
 #include <unistd.h>
 #include "../include/sysmonitor.h"
 
+// Retrieves memory statistics
 int parse_mem(mem_stat_t *mem){
     FILE *fp = fopen("/proc/meminfo", "r");
     if (fp == NULL){
         perror("Error: failed to open /proc/meminfo [mem.c parse_mem()]");
         exit(EXIT_FAILURE);
     }
-
+    // Reads and retrieves memory statistics
     char line[512];
     int read = 0;
     while( fgets(line, sizeof(line), fp) != NULL) {
@@ -28,13 +29,13 @@ int parse_mem(mem_stat_t *mem){
         }
     }
 
-    // Testa la causa del fallimento della lettura
+    // Checks potential causes of read failure
     if (ferror(fp)) {
         perror("fgets");
         fclose(fp);
         return EXIT_FAILURE;
     }
-	// Chiusura del file e gestione dell'eventuale fallimento
+	// Closes the file and handles errors, if they occur
     if (fclose(fp) != 0) {
         perror("fclose");
         return EXIT_FAILURE;
@@ -42,13 +43,14 @@ int parse_mem(mem_stat_t *mem){
     return EXIT_SUCCESS;
 }
 
+// Prints memory statistics
 void print_mem(mem_stat_t *mem){
     printf("Total memory: %12llu KB\n", mem->mem_total);
     printf("Free memory: %13llu KB\n", mem->mem_free);
     printf("Available memory: %8llu KB\n", mem->mem_available);
 }
 
-// Function that gets memory data and prints them
+// Retrieves memory statistics and prints them
 void get_memory_usage(){
     mem_stat_t mem;
     parse_mem(&mem);
