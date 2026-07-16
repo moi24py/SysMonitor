528ab2276971909a75654f20a6e95161844c71dd
moi24py <moi24py@gmail.com>
2026-07-16 14:17:47 +0200

    add logic to filter pseudo-filesystems and function to print disk_t arrays

--- src/disk.c
+++ src/disk.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
+#include <sys/statvfs.h>
 #include "../include/sysmonitor.h"
 
 // Array that lists pseudo-filesystem types
-const char *ignored_fstype[] = {
+const char *pseudo_fstype[] = {
     "proc",
     "sysfs",
     "devpts",
@@ -25,20 +27,25 @@ const char *ignored_fstype[] = {
 size_t const size_ignored_fstype = 15;
 
 // Checks if the filesystem type is virtual
-bool is_ignored_fstype(char *fsname){
+bool is_pseudo_fstype(char *fsname){
     size_t i;
     bool is_ign = false;
     for(i=0; i<size_ignored_fstype; i++){
-        if ( strncmp(fsname, ignored_fstype[i], strlen(fsname)) == 0 ){
-            printf("UGUALI: [fsname: %s] [ignored_fstype: %s]\n", fsname, ignored_fstype[i]);
-            is_ign = true;
-        }
-        else {
-            printf("diversi: [fsname: %s] [ignored_fstype: %s]\n", fsname, ignored_fstype[i]);
-            is_ign = false;
+        if ( strncmp(fsname, pseudo_fstype[i], strlen(fsname)) == 0 ){
+            //printf("UGUALI: [fsname: %s] [ignored_fstype: %s]\n\n", fsname, ignored_fstype[i]);
+            return true;
         }
     }
-    return is_ign;
+    return false;
+}
+
+void print_fs_array(disk_t *fs){
+    printf("        FSTYPE                   PSEUDO      MOUNT POINT\n");
+    for(size_t i=0; i<MAX_FS; i++){
+        char s = (fs+i)->fstype[0];
+        if (s != '\0' &&  isalpha(s))
+            printf("#%-3ld    %-20s     %d           %s\n", i+1, (fs+i)->fstype, (fs+i)->pseudo, (fs+i)->mount);
+    }
 }
 
 // Retrieves mounted filesystems (pseudo-fs excluded)
@@ -50,7 +57,8 @@ int retrieve_fs(){
     }
 
     // Struct that stores filesystems name and mount point 
-    disk_t fsys[100];
+    disk_t fsys[MAX_FS];
+
     char line[1024];
     int fsys_qty = 0;
     int num = 1;
@@ -59,13 +67,14 @@ int retrieve_fs(){
         // Filters out mounts with virtual filesystem fstype (e.g., procfs/sysfs/tmpfs)
         // so the disk report focuses on real storage. Optionally includes virtual filesystems
         sscanf(line, "%*s %s %s", fsys[fsys_qty].mount, fsys[fsys_qty].fstype);
-        printf("%2d ] fstype: %s\n", num++, fsys[fsys_qty].fstype);
-        printf("     mount point:%s\n", fsys[fsys_qty].mount);
-        fsys[fsys_qty].fstype[strlen(fsys[fsys_qty].fstype)+1] = '\0';
-        bool ign = is_ignored_fstype(fsys[fsys_qty].fstype);
-        printf("%d\n", ign);
+        //printf("%2d ] fstype: %s\n", num++, fsys[fsys_qty].fstype);
+        //printf("     mount point:%s\n", fsys[fsys_qty].mount);
+        if (is_pseudo_fstype(fsys[fsys_qty].fstype)) fsys[fsys_qty].pseudo = true;
+        else fsys[fsys_qty].pseudo = false;
         fsys_qty++;
     }
+
+    print_fs_array(fsys);
 
     if(ferror(fp)){
         perror("Error: failed to read /proc/mounts");
