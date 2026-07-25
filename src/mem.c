208a0b8a89e6ecd9a394eab999d1c5c1d7ac4894
moi24py <moi24py@gmail.com>
2026-07-25 14:45:26 +0200

    update comments

--- src/mem.c
+++ src/mem.c
@@ -52,6 +52,7 @@ void print_mem(mem_stat_t *mem){
 
 // Retrieves memory statistics and prints them
 void get_memory_usage(void){
+    printf("MEMORY USAGE\n");
     mem_stat_t mem;
     parse_mem(&mem);
     print_mem(&mem);
